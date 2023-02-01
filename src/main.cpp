e529f84abe5f3233e75fc6b59eed0073a41d1d29
person4268 <28717044+person4268@users.noreply.github.com>
2023-01-31 20:10:25 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "I2C.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  I2c.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
